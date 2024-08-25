b242ea12e64d3459a9fadbaa734eb87fd6439455
annacwu <annacwu04@gmail.com>
2024-08-25 16:06:18 -0300

    Initial Commit

---
+++ Habite/HabiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabiteApp.swift
+//  Habite
+//
+//  Created by Anna Wu on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct HabiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
